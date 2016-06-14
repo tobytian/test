14a71d163ce171526ead34d624558963d5942d48
Hiram <hiramtan@live.com>
2016-06-14 16:48:10 +0800

    使用renderertexture绘制

---
+++ unity/Assets/NewBehaviourScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    public RenderTexture t;
+
+    void Update()
+    {
+        GetComponent<Renderer>().material.mainTexture = t;
+    }
+
+    void OnGUI()
+    {
+        GUI.DrawTexture(new Rect(0, 0, 100, 100), t);
+    }
+}
