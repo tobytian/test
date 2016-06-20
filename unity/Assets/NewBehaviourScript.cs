e9d3aa0274a2112a46a709245e2365d7ef641da9
Hiram <hiramtan@live.com>
2016-06-20 15:52:28 +0800

    测试转向坐标和转向目标物体逻辑

---
+++ unity/Assets/NewBehaviourScript.cs
@@ -0,0 +1,26 @@
+﻿//*********************************************************************
+// Description:
+// Author: hiramtan@live.com
+//*********************************************************************
+using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public Transform test;
+	// Use this for initialization
+	void Start () {
+	
+        Hashtable ht = new Hashtable();
+
+        ht.Add("looktarget", test);
+
+        iTween.LookTo(gameObject,ht);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
