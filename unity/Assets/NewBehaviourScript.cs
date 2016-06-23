71b9b4dc21c6d6675c4e28c38ee762eeefafe9d6
Hiram <hiramtan@live.com>
2016-06-23 16:10:51 +0800

    测试抛物线

--- unity/Assets/NewBehaviourScript.cs
+++ unity/Assets/NewBehaviourScript.cs
@@ -15,20 +15,44 @@ public class NewBehaviourScript : MonoBehaviour
         lr = gameObject.GetComponent<LineRenderer>();
 
 
-        int radius = 30;
 
-        int count = 100;
-        float eachAngle = 360f/count;
-        Vector3 forward = transform.forward;
 
-        lr.SetVertexCount(count+1);
-        for (int i = 0; i <= count; i++)
+
+        Bezier bezier = new Bezier(Vector3.left*10, Vector3.up*10,Vector3.down*10, Vector3.right *10);
+        lr.SetVertexCount(100);
+
+
+        for (int i = 1; i <= 100; i++)
         {
-            Vector3 pos = Quaternion.Euler(0, eachAngle*i, 0)*forward*radius + transform.position;
-            lr.SetPosition(i,pos);
+            Vector3 vec = bezier.GetPointAtTime((float)(i * 0.01));
+            lr.SetPosition(i - 1, vec);
         }
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //int radius = 30;
+        //int count = 100;
+        //float eachAngle = 360f/count;
+        //Vector3 forward = transform.forward;
+        //lr.SetVertexCount(count+1);
+        //for (int i = 0; i <= count; i++)
+        //{
+        //    Vector3 pos = Quaternion.Euler(0, eachAngle*i, 0)*forward*radius + transform.position;
+        //    lr.SetPosition(i,pos);
+        //}
     }
 
     // Update is called once per frame
---
+++ unity/Assets/ProjectileTest.cs
@@ -0,0 +1,44 @@
+﻿//*********************************************************************
+// Description:
+// Author: hiramtan@live.com
+//*********************************************************************
+using UnityEngine;
+using System.Collections;
+/// <summary>
+/// 弓箭轨迹模拟
+/// 阿亮设计，欢迎交流经验
+/// </summary>
+public class TestRay : MonoBehaviour
+{
+
+    public float Power = 10;//这个代表发射时的速度/力度等，可以通过此来模拟不同的力大小
+    public float Angle = 45;//发射的角度，这个就不用解释了吧
+    public float Gravity = -10;//这个代表重力加速度
+
+
+
+    private Vector3 MoveSpeed;//初速度向量
+    private Vector3 GritySpeed = Vector3.zero;//重力的速度向量，t时为0
+    private float dTime;//已经过去的时间
+    // Use this for initialization
+    void Start()
+    {
+        //通过一个公式计算出初速度向量
+        //角度*力度
+        MoveSpeed = Quaternion.Euler(new Vector3(-Angle, 0, 0)) * Vector3.forward * Power;
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        //计算物体的重力速度
+        //v = at ;
+        GritySpeed.y = Gravity * (dTime += Time.fixedDeltaTime);
+        //位移模拟轨迹
+        transform.Translate(MoveSpeed * Time.fixedDeltaTime);
+        transform.Translate(GritySpeed * Time.fixedDeltaTime);
+
+    }
+}
