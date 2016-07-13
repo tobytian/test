b04555ea8da69e1561fb91c96a0eec5a7aea4450
Hiram <hiramtan@live.com>
2016-07-13 12:16:10 +0800

    测试对象池

---
+++ unity/Assets/NewBehaviourScript.cs
@@ -0,0 +1,32 @@
+﻿//*********************************************************************
+// Description:
+// Author: hiramtan@live.com
+//*********************************************************************
+using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    private int count=10;
+
+
+    void Start()
+    {
+       // InvokeRepeating("Test", 0, 0.5f);
+    }
+
+    void Test()
+    {
+        for (int i = 0; i < count; i++)
+            Instantiate(Resources.Load("go"));
+
+
+      
+    }
+
+    void Update()
+    {
+        Test();
+    }
+}
---
+++ unity/Assets/NewBehaviourScript1.cs
@@ -0,0 +1,23 @@
+﻿//*********************************************************************
+// Description:
+// Author: hiramtan@live.com
+//*********************************************************************
+using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript1 : MonoBehaviour
+{
+
+    private bool isUsed;
+	// Use this for initialization
+	void Start () {
+
+
+        Destroy(gameObject, 0.1f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
