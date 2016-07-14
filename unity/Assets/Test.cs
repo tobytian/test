1bc73d3fcda4cff0c26e00d6f5fd1af200046ad7
Hiram <hiramtan@live.com>
2016-07-14 19:06:34 +0800

    一张光照贴图

---
+++ unity/Assets/Test.cs
@@ -0,0 +1,33 @@
+﻿//*********************************************************************
+// Description:
+// Author: hiramtan@live.com
+//*********************************************************************
+using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        Debuger.fontSize = 20;
+        Debuger.EnableOnScreen(true);
+
+
+
+        var test = LightmapSettings.lightmaps;
+        Debuger.Log(test.Length);
+
+        LightmapData test2 = test[0];
+        Debuger.Log(test2.lightmapFar.name);
+        Debuger.Log(test2.lightmapNear.name);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
