e71ba16bf6a893e51c606f606fab840d391b04d7
Hiram <hiramtan@live.com>
2016-07-20 21:07:43 +0800

    添加render

---
+++ unity/Assets/Fog of War/FoW/New Folder/HiFowRender.cs
@@ -0,0 +1,62 @@
+﻿//*********************************************************************
+// Description:
+// Author: hiramtan@live.com
+//*********************************************************************
+using UnityEngine;
+using System.Collections;
+
+public class HiFowRender : MonoBehaviour
+{
+    public float updateRate = 0.1f;
+    private Renderer[] renderers;
+    private float nextUpdate;
+    public bool isVisible { get; private set; }
+    private bool isUpdate = true;
+
+
+    // Use this for initialization
+	void Start ()
+	{
+	    renderers = GetComponentsInChildren<Renderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Rebuild()
+    {
+        isUpdate = true;
+    }
+
+    void LateUpdate()
+    {
+        if (nextUpdate < Time.time)
+            UpdateNow();
+    }
+
+    void UpdateNow()
+    {
+        nextUpdate = Time.time + updateRate;
+        if (!HiFowSystem.Instance)
+        {
+            enabled = false;
+            return;
+        }
+        bool temp = HiFowSystem.Instance.IsVisible(transform.position);
+        if (isUpdate||isVisible!=temp)
+        {
+            isUpdate = false;
+            isVisible = temp;
+            for (int i = 0; i < renderers.Length; i++)
+            {
+                Renderer tempRender = renderers[i];
+                if (tempRender)
+                    tempRender.enabled = isVisible;
+            }
+            isUpdate = true;
+            nextUpdate = Time.time;
+        }
+    }
+}
