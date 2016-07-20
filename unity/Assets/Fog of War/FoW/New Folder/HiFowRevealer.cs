e3e750ae8bb94fdd06df803dc72faae21acdf88b
Hiram <hiramtan@live.com>
2016-07-20 21:17:38 +0800

    完成revealer

---
+++ unity/Assets/Fog of War/FoW/New Folder/HiFowRevealer.cs
@@ -0,0 +1,52 @@
+﻿//*********************************************************************
+// Description:
+// Author: hiramtan@live.com
+//*********************************************************************
+using UnityEngine;
+using System.Collections;
+
+public class HiFowRevealer : MonoBehaviour
+{
+
+    public float range = 50;
+
+    public bool isActive = true;
+
+    private HiFowSystem.Revealer revealer;
+
+    void Awake()
+    {
+        revealer = HiFowSystem.CreateRevealer();
+    }
+
+    void OnDisable()
+    {
+        revealer.isActive = false;
+    }
+
+    void OnDestroy()
+    {
+        HiFowSystem.DeleteRevealer(revealer);
+        revealer = null;
+    }
+
+    void LateUpdate()
+    {
+        if (isActive)
+        {
+            revealer.pos = transform.position;
+            revealer.range = range;
+            revealer.isActive = true;
+        }
+        else
+        {
+            revealer.isActive = false;
+        }
+        revealer.cachedBuffer = null;
+    }
+
+    public void Rebuild()
+    {
+        revealer.cachedBuffer = null;
+    }
+}
