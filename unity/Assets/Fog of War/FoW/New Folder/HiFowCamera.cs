169bbe74f4adbac03f9f0da1e305a89dc85e77a7
Hiram <hiramtan@live.com>
2016-07-20 21:45:12 +0800

    简化逻辑:一直刷新,不需要高度信息,优化性能

---
+++ unity/Assets/Fog of War/FoW/New Folder/HiFowCamera.cs
@@ -0,0 +1,65 @@
+﻿//*********************************************************************
+// Description:
+// Author: hiramtan@live.com
+//*********************************************************************
+using UnityEngine;
+using System.Collections;
+
+public class HiFowCamera : MonoBehaviour
+{
+    public Color unexplored = Color.gray;
+    //public Color explored = Color.gray;
+
+    private Shader shader;
+    private HiFowSystem fOWSystem;
+    private Camera camera;
+    private Matrix4x4 inverseMVP;
+    private Material material;
+
+    
+
+    // Use this for initialization
+    void Start()
+    {
+        if (!SystemInfo.supportsImageEffects || !shader || !shader.isSupported)
+        {
+            enabled = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnEnable()
+    {
+        camera = GetComponent<Camera>();
+        camera.depthTextureMode = DepthTextureMode.Depth;
+        if (!shader)
+            shader = Shader.Find("Image Effects/Fog of War");
+    }
+
+    void OnDisable()
+    {
+        if (material)
+            DestroyImmediate(material);
+    }
+
+    void OnRenderImage(RenderTexture paramSource,RenderTexture paramDestination)
+    {
+        if (!fOWSystem)
+        {
+            fOWSystem = HiFowSystem.Instance;
+            if (!fOWSystem)
+                fOWSystem = FindObjectOfType<HiFowSystem>();
+        }
+        if (!fOWSystem || !fOWSystem.enabled)
+        {
+            enabled = false;
+            return;
+        }
+
+        inverseMVP = (camera.projectionMatrix*camera.worldToCameraMatrix).inverse;
+    }
+}
---
+++ unity/Assets/Fog of War/FoW/New Folder/HiFowSystem.cs
@@ -0,0 +1,91 @@
+﻿//*********************************************************************
+// Description:
+// Author: hiramtan@live.com
+//*********************************************************************
+using UnityEngine;
+using System.Collections;
+
+public class HiFowSystem : MonoBehaviour
+{
+
+    public static HiFowSystem Instance;
+
+
+
+    static BetterList<Revealer> addedList = new BetterList<Revealer>();
+    static BetterList<Revealer> removedList = new BetterList<Revealer>();
+
+
+
+    private Color32[] buffer0Color32;
+    private Color32[] buffer1Color32;
+    public int worldSize = 512;
+    public int textureSize = 128;
+
+    private Vector3 originPos;
+
+    // Use this for initialization
+    void Start()
+    {
+        #region 设置原点
+        originPos = transform.position;
+        originPos.x -= worldSize * 0.5f;
+        originPos.z -= worldSize * 0.5f;
+        #endregion
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public static Revealer CreateRevealer()
+    {
+        Revealer temp = new Revealer();
+        temp.isActive = false;
+        lock (addedList)
+        {
+            addedList.Add(temp);
+        }
+        return temp;
+    }
+
+    public static void DeleteRevealer(Revealer param)
+    {
+        lock (removedList)
+        {
+            removedList.Add(param);
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="param"></param>
+    /// <returns>position</returns>
+    public bool IsVisible(Vector3 param)
+    {
+        if (buffer0Color32 == null)
+            return false;
+        param -= originPos;
+        float tempWorldToTex = (float)textureSize / worldSize;
+        int tempCx = Mathf.RoundToInt(param.x * tempWorldToTex);
+        int tempCz = Mathf.RoundToInt(param.z * tempWorldToTex);
+        tempCx = Mathf.Clamp(tempCx, 0, textureSize - 1);
+        tempCz = Mathf.Clamp(tempCz, 0, textureSize - 1);
+        int tempIndex = tempCx + tempCz * textureSize;
+        return buffer0Color32[tempIndex].r > 0 || buffer1Color32[tempIndex].r > 0;
+    }
+
+    public class Revealer
+    {
+        public bool isActive;
+        public Vector3 pos;
+        public float range;
+        public bool[] cachedBuffer;
+        public int cachedSize;
+        public int cachedX;
+        public int cachedY;
+    }
+}
