07d000ca3f46f6dc4f9478d703ce3ed07b567495
Hiram <hriamtan@qq.com>
2017-06-07 12:13:22 +0800

    测试引用类型值类型

---
+++ unity/Assets/NewBehaviourScript.cs
@@ -0,0 +1,44 @@
+﻿//*********************************************************************
+// Description:
+// Author: hiramtan@live.com
+//*********************************************************************
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    public List<GameObject> test = new List<GameObject>();
+
+
+    public List<GameObject> test1;
+
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(0, 0, 100, 50), "click"))
+            test.RemoveAt(0);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        test.Add(new GameObject());
+        test.Add(new GameObject());
+        test.Add(new GameObject());
+
+
+        test1 = new List<GameObject>(test);
+
+        // var test
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.LogError(test.Count + "---" + test1.Count);
+    }
+}
