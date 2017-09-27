ea61634a8771bd7e559f2b506bd3410176c85c5a
Hiram <hriamtan@qq.com>
2017-09-27 15:53:35 +0800

    发包测试

---
+++ unity/Assets/Editor/TestBuild.cs
@@ -0,0 +1,32 @@
+﻿//****************************************************************************
+// Description:
+// Author: hiramtan@qq.com
+//***************************************************************************
+
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestBuild : MonoBehaviour
+{
+    [MenuItem("Jenkins/Build")]
+    private static void Build()
+    {
+        EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTarget.Android);
+        var Array = EditorBuildSettings.scenes;
+        BuildPipeline.BuildPlayer(Array, "./test.apk", BuildTarget.Android, BuildOptions.None);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ unity/Assets/NewBehaviourScript.cs
@@ -0,0 +1,16 @@
+﻿//****************************************************************************
+// Description:
+// Author: hiramtan@qq.com
+//***************************************************************************
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    void OnGUI()
+    {
+       GUI.Label(new Rect(0,0,500,300),"UI" );
+    }
+}
