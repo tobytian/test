40af6f38139c2a4d16495b258dc229f3b1de44e9
Hiram <hriamtan@qq.com>
2017-10-17 18:30:44 +0800

    添加测试ping逻辑

---
+++ unity/Assets/NewBehaviourScript.cs
@@ -0,0 +1,36 @@
+﻿//****************************************************************************
+// Description:
+// Author: hiramtan@qq.com
+//***************************************************************************
+
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using UnityEngine;
+using Ping = UnityEngine.Ping;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private string ip = "122.11.58.160";
+    private int pingTime;
+    private Ping p;
+    private float timeOut = 1;
+    private float lastTime;
+    void Start()
+    {
+        StartCoroutine(Ping());
+    }
+    IEnumerator Ping()
+    {
+        p = new Ping("127.0.0.1");
+        lastTime = Time.realtimeSinceStartup;
+        while (!p.isDone && Time.realtimeSinceStartup - lastTime < 1)
+        {
+            yield return null;
+        }
+        pingTime = p.time;
+        p.DestroyPing();
+        yield return new WaitForSeconds(1);//一秒更新一次
+        StartCoroutine(Ping());
+    }
+}
