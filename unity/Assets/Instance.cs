0161e56b3a57a23543180b62317ba5f9a7861411
Hiram <hriamtan@qq.com>
2017-10-17 18:57:09 +0800

    测试示例创建

---
+++ unity/Assets/Instance.cs
@@ -0,0 +1,34 @@
+﻿//****************************************************************************
+// Description:
+// Author: hiramtan@qq.com
+//***************************************************************************
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instance : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        var t = typeof(Test);
+
+        Debug.Log(t);
+
+        var tt = Activator.CreateInstance(t);
+        var ttt = tt as Test;
+        ttt.L();
+    }
+}
+
+
+public class Test
+{
+    public void L()
+    {
+        Debug.Log("execute");
+    }
+}
