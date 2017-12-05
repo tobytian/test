20f3b9fef6c195e2d4a13f9d13d6a89645603bea
Hiram <hriamtan@qq.com>
2017-12-05 16:34:03 +0800

    添加摇杆判定

---
+++ unity/Assets/Script/HiFloat.cs
@@ -0,0 +1,13 @@
+﻿//****************************************************************************
+// Description:
+// Author: hiramtan@live.com
+//****************************************************************************
+
+public class HiFloat
+{
+    public static float FloatEqualJudgeValue = 0.1f;//小于认为相等
+    public static bool IsEqual(float f1, float f2)
+    {
+        return System.Math.Abs(f1 - f2) < FloatEqualJudgeValue;
+    }
+}
---
+++ unity/Assets/Script/Joystick.cs
@@ -0,0 +1,73 @@
+﻿//****************************************************************************
+// Description:
+// Author: hiramtan@live.com
+//****************************************************************************
+
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Joystick : ScrollRect
+{
+    private float _recoverTime = 0.1f;//摇杆恢复时间
+    private float _radius;
+    private Vector3 _contentOffset = Vector3.zero;
+    private EState state = EState.None;
+    enum EState
+    {
+        None,
+        Start,//开始拖拽
+        Dragging,//正在拖拽
+        End,//结束拖拽
+    }
+
+    void Start()
+    {
+        _radius = (transform as RectTransform).sizeDelta.x * 0.5f;
+    }
+
+    public override void OnBeginDrag(PointerEventData eventData)
+    {
+        base.OnBeginDrag(eventData);
+        state = EState.Start;
+    }
+
+    public override void OnEndDrag(PointerEventData eventData)
+    {
+        base.OnEndDrag(eventData);
+        state = EState.End;
+    }
+
+    public override void OnDrag(PointerEventData eventData)
+    {
+        base.OnDrag(eventData);
+        state = EState.Dragging;
+        var contentPostion = this.content.anchoredPosition;
+        if (contentPostion.magnitude > _radius)
+        {
+            contentPostion = contentPostion.normalized * _radius;
+            SetContentAnchoredPosition(contentPostion);
+        }
+    }
+    void Update()
+    {
+        RecoverContent();
+    }
+
+    void RecoverContent()
+    {
+        if (state == EState.End)
+        {
+            if (HiFloat.IsEqual(content.localPosition.x,0)&&HiFloat.IsEqual(content.localPosition.y,0))
+                state = EState.None;
+            float x = Mathf.Lerp(content.localPosition.x, 0.0f, _recoverTime);
+            float y = Mathf.Lerp(content.localPosition.y, 0.0f, _recoverTime);
+            content.localPosition = new Vector3(x, y, content.localPosition.z);
+        }
+    }
+
+    void UpdateOffset()
+    {
+        _contentOffset = content.localPosition / _radius;
+    }
+}
