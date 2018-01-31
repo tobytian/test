6abedbbaccc164b9591ddcefdccdad21f0f8061a
Hiram <hriamtan@qq.com>
2018-01-31 15:17:26 +0800

    test memory

--- unity/Assets/NewBehaviourScript.cs
+++ unity/Assets/NewBehaviourScript.cs
@@ -3,51 +3,19 @@
 // Author: hiramtan@live.com
 //****************************************************************************
 
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
-{
+public class NewBehaviourScript : MonoBehaviour {
 
-    // Use this for initialization
-    void Start()
-    {
-
-
-
-        var t = Activator.CreateInstance(typeof(Test4));
-        Debug.Log(t);
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-}
-
-public class Test1
-{
-}
-
-public class Test2
-{
-}
-
-public class Test3
-{
-
-}
-
-public class Test4
-{
-    public Test4()
-    { }
-    public Test4(Test1 t1, Test2 t2)
-    {
-        Debug.Log(t1);
-    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
 }
