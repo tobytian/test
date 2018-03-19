36d82af30e6e7ddcde0a4baf6ebd053425c7eef2
Hiram <hriamtan@live.com>
2018-03-19 16:12:42 +0800

    2. add reactive system

--- unity/Assets/Script/Main.cs
+++ unity/Assets/Script/Main.cs
@@ -9,7 +9,7 @@ using UnityEngine;
 
 public class Main : MonoBehaviour
 {
-
+    private TestLogSystem s;
     // Use this for initialization
     void Start()
     {
@@ -17,11 +17,14 @@ public class Main : MonoBehaviour
         var e = context.CreateEntity();
         e.AddHp(100);
 
+
+        s =  new TestLogSystem(context);
+        s.Execute();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        s.Execute();
     }
 }
---
+++ unity/Assets/Script/System/LogSystem.cs
@@ -0,0 +1,34 @@
+﻿//****************************************************************************
+// Description:
+// Author: hiramtan@live.com
+//****************************************************************************
+
+using System.Collections;
+using System.Collections.Generic;
+using Entitas;
+using UnityEngine;
+
+public class TestLogSystem:ReactiveSystem<GameEntity>
+{
+    public TestLogSystem(IContext<GameEntity> context) : base(context)
+    {
+    }
+
+    protected override ICollector<GameEntity> GetTrigger(IContext<GameEntity> context)
+    {
+         return context.CreateCollector(GameMatcher.Hp);
+    }
+
+    protected override bool Filter(GameEntity entity)
+    {
+        return entity.hasHp;
+    }
+
+    protected override void Execute(List<GameEntity> entities)
+    {
+        foreach (var VARIABLE in entities)
+        {
+            Debug.Log(VARIABLE.hp.hp);
+        }
+    }
+}
