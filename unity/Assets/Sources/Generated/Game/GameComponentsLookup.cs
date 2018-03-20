6940db6a35541734a1be7217799c55bf2aea3587
Hiram <hriamtan@live.com>
2018-03-20 18:24:55 +0800

    only entitas basic code

--- unity/Assets/Sources/Generated/Game/GameComponentsLookup.cs
+++ unity/Assets/Sources/Generated/Game/GameComponentsLookup.cs
@@ -8,15 +8,15 @@
 //------------------------------------------------------------------------------
 public static class GameComponentsLookup {
 
-    public const int HelloWorld = 0;
 
-    public const int TotalComponents = 1;
+
+    public const int TotalComponents = 0;
 
     public static readonly string[] componentNames = {
-        "HelloWorld"
+
     };
 
     public static readonly System.Type[] componentTypes = {
-        typeof(HelloWorldComponent)
+
     };
 }
