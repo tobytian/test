33e625a3c171596562935639d34bc9635640b615
Hiram <hriamtan@live.com>
2018-03-21 16:00:59 +0800

    add example and resources

--- unity/Assets/Script/Main.cs
+++ unity/Assets/Script/Main.cs
@@ -3,19 +3,35 @@
  *
  * Author: hiramtan@live.com
  ****************************************************************************/
-using System.Collections;
-using System.Collections.Generic;
+using Entitas;
 using UnityEngine;
 
-public class Main : MonoBehaviour {
+public class Main : MonoBehaviour
+{
+    private Systems _systems;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private Contexts _contexts;
+    // Use this for initialization
+    void Start()
+    {
+        _contexts = Contexts.sharedInstance;
+        _systems = CreateSystems(_contexts);
+        _systems.Initialize();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _systems.Execute();
+        _systems.Cleanup();
+    }
+
+    Systems CreateSystems(Contexts contexts)
+    {
+        return new Feature("Systems")
+            .Add(new InputSystems(contexts))
+            .Add(new MovementSystems(contexts))
+            .Add(new ViewSystems(contexts));
+    }
 }
