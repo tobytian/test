5dc6aca3f938e3ef4a29eecdf13ed6c5f0783e64
Hiram <hriamtan@live.com>
2018-03-21 15:30:31 +0800

    add input controller

---
+++ unity/Assets/Script/Systems/CommandMoveSystem.cs
@@ -0,0 +1,41 @@
+﻿/****************************************************************************
+ * Description:
+ *
+ * Author: hiramtan@live.com
+ ****************************************************************************/
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Entitas;
+
+public class CommandMoveSystem:ReactiveSystem<InputEntity>
+{
+    private GameContext _gameContext;
+    private IGroup<GameEntity> _movers;
+
+    public CommandMoveSystem(Contexts contexts) : base(contexts.input)
+    {
+        _movers = contexts.game.GetGroup(GameMatcher.AllOf(GameMatcher.Mover).NoneOf(GameMatcher.Move));
+    }
+
+    protected override ICollector<InputEntity> GetTrigger(IContext<InputEntity> context)
+    {
+        return context.CreateCollector(InputMatcher.AllOf(InputMatcher.LeftMouse, InputMatcher.MouseDown));
+    }
+
+    protected override bool Filter(InputEntity entity)
+    {
+        return entity.hasMouseDown;
+    }
+
+    protected override void Execute(List<InputEntity> entities)
+    {
+        foreach (var e in entities)
+        {
+            GameEntity[] mover = _movers.GetEntities();
+            if (mover.Length <= 0) return;
+            mover[Random.Range(0,mover.Length)].ReplaceMove(e.mouseDown.postion);
+
+        }
+    }
+}
---
+++ unity/Assets/Script/Systems/InputSystems.cs
@@ -0,0 +1,18 @@
+﻿/****************************************************************************
+ * Description:
+ *
+ * Author: hiramtan@live.com
+ ****************************************************************************/
+
+using UnityEngine;
+using System.Collections;
+
+public class InputSystems : Feature
+{
+    public InputSystems(Contexts contexts) : base("input systems")
+    {
+        Add(new EmitInputSystem(contexts));
+        Add(new CreateMoverSystem(contexts));
+        Add(new CommandMoveSystem(contexts));
+    }
+}
