88886f9bdac54a9457d480381392181d84a366e7
Hiram <hriamtan@live.com>
2018-05-02 16:54:53 +0800

    test bit

---
+++ unity/Assets/NewBehaviourScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        int[] test = new[] { 1 };
+        BitArray bitArray = new BitArray(test);
+        Debug.Log(bitArray.Count);
+        string s = "";
+        for (int i = bitArray.Count; i > 0; i--)
+        {
+            var value = bitArray[i - 1] ? 1 : 0;
+            s += value;
+        }
+        Debug.Log(s);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
