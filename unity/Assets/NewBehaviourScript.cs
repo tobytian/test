a86894753f285bbe91605062c036ccb05b7de8de
Hiram <hriamtan@live.com>
2018-05-02 18:08:21 +0800

    test thread

--- unity/Assets/NewBehaviourScript.cs
+++ unity/Assets/NewBehaviourScript.cs
@@ -1,28 +1,9 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
+using System.Threading;
 using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
 
-    // Use this for initialization
-    void Start()
-    {
-        int[] test = new[] { 1 };
-        BitArray bitArray = new BitArray(test);
-        Debug.Log(bitArray.Count);
-        string s = "";
-        for (int i = bitArray.Count; i > 0; i--)
-        {
-            var value = bitArray[i - 1] ? 1 : 0;
-            s += value;
-        }
-        Debug.Log(s);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
+    
 }
