478ea6785ed95a68a8ed76f56a985392d8af0d77
Hiram <hriamtan@live.com>
2018-05-03 16:56:15 +0800

    添加bit 数据结构

--- unity/Assets/NewBehaviourScript.cs
+++ unity/Assets/NewBehaviourScript.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,26 +9,57 @@ public class NewBehaviourScript : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-        int[] test = new[] {1 };
-        BitArray bitArray = new BitArray(test);
+    }
+}
 
-        int[] test1 = new[] { 2 };
-        BitArray bitArray1 = new BitArray(test1);
 
-        var result = bitArray.Or(bitArray1);
-
-        string s = "";
-        for (int i = result.Count; i > 0; i--)
+public class Bit
+{
+    private byte[] _bytes;
+    private BitArray _bitArray;
+    private const int BitsOfOneByte = 8;
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="bitCount">how many bit</param>
+    public Bit(int bitCount = BitsOfOneByte)
+    {
+        if (bitCount <= 0)
         {
-            var value = result[i - 1] ? 1 : 0;
-            s += value;
+            throw new Exception("bit count can not less or equal 0");
         }
-        Debug.Log(s);
+        var @base = bitCount / BitsOfOneByte;
+        var left = bitCount % BitsOfOneByte;
+        var length = left == 0 ? @base : @base + 1;
+        if (length <= 0)
+        {
+            throw new Exception("byte array can not less or equal 0");
+        }
+        _bytes = new byte[length];
+        _bytes[0] = 1;
+        _bitArray = new BitArray(_bytes);
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// 向左移位
+    /// </summary>
+    /// <param name="count"></param>
+    public void MoveLeft(int count)
     {
+        var index = count / BitsOfOneByte;
 
+        BitArray b = new BitArray(_bitArray.Length);
+
+    }
+
+    public string GetBit()
+    {
+        string s = "";
+        for (int i = _bitArray.Length; i > 0; i--)
+        {
+            var value = _bitArray[i - 1] ? 1 : 0;
+            s += value;
+        }
+        return s;
     }
 }
